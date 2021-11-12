412c68fcfb7fde895c6ec04168a106f593be317a
Douglas Wilson <dougwilson0000@gmail.com>
2021-11-12 14:49:27 -0500

    completed task 2 Counter

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+//because nestedFunction is a closure and retains access to the variable created in the outer function.
 
 
 
@@ -28,11 +28,14 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(num) {
+  let counter = 0;
+  for(let i = 1; i < num + 1; i++) {
+    counter += i;
   }
- 
+  return counter;
+}
+ console.log(summation(10));
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
